06fc48f371ac50d36264c288d1b049259c8eb4cf
retuzhamidulloh <retuz416@gmail.com>
2025-08-12 14:32:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let bosh = alert ("Boshidagi array uzunligi 4")
+console.log(alert);
+
+let arr = ['olma', 'banan', 'gilos', 'shoftoli'];
+console.log(arr);
+let siz = alert  ("siznig arrayigizdan malumotlarni olib tashlamoqchimisiz")
+let arra = ['olma', 'banan', 'gilos', 'shoftoli'];
+console.log(arra);
+
+let oxirgiMeva = arra.pop();
+
+let array = alert("Array uzunligi:3")
+console.log(oxirgiMeva);
+      
